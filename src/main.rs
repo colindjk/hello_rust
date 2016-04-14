47403f71d12d36c132ff727052c92b8711cb971c
colindjk <colindjk@gmail.com>
2016-04-14 14:17:10 -0400

    Got the guessing game to work, next I will put it on a seperate file / function to be called by main at will

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,40 @@
 extern crate rand;
 
 use std::io;
-use std::comp::Ordering;
+use std::cmp::Ordering;
 use rand::Rng;
 
 /// Random number guessing game.
 fn main()
 {
     println!("Guess the number");
+    // Below defaults to i32 (signed 32-bit integer)
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("The secret_number is {}", secret_number);
-    println!("Please input your guess:");
+    loop {
 
-    let mut guess = String::new();
+        println!("Please input your guess:");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
+        // so, parse() returns an enum, with the 'Ok' type or the 'Err' type.
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num, // if Okay, write to 'num', return num.
+            Err(_) => continue, // continue: next iter of 'loop'
+        };
 
+        println!("You guessed {}", guess);
 
+        // The 'Ordering' value is an 'enum'; which looks like this: enum foo { Bar, Biz }; 
+        // where Bar and Biz are different types of Foo
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
